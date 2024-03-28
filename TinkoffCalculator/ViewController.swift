f7853186cbfcb4be4604b5296e5a322e98d1ba25
Sergei Kornilov <creshez@gmail.com>
2024-03-28 15:48:33 +0100

    first commit2

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -8,19 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBAction func buttonPressed(_ sender: UIButton) {
-        guard let buttonText = sender.currentTitle else { return}
-        
+        guard let buttonText = sender.currentTitle else { return }
         print(buttonText)
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Та-дам!")
     }
-
 
 }
 
